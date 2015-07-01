ffad1589253950402a45aaf6b36ebdfb1e999632
Shanksum <mariusnisslmuller@yahoo.de>
2015-07-01 15:53:29 +0200

    Neuer Ressourcen Austausch

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -105,48 +105,26 @@ public class NPC : MonoBehaviour, INPC {
 		return nahrungTragend;
 	}
 
-	//einheiten werden in zehner schritten übergeben
-	public bool AddRessourceTragend(int anzahl, RessourceType ressource){
+
+
+	public bool SetTragend(int neuDazu, RessourceType ressource){
 		bool erfolg = false;
-		if (ressource == RessourceType.Holz) {
-			erfolg = SetHolzTragend(anzahl);
-		}
-		if (ressource == RessourceType.Stein) {
-			erfolg = SetSteinTragend(anzahl);
-		}
-		if (ressource == RessourceType.Nahrung) {
-			erfolg = SetNahrungTragend(anzahl);
+		if ((trageStatus + neuDazu) <= kapazitaet && (trageStatus + neuDazu) >= 0) {
+			if(RessourceType.Holz){
+				holzTragend += neuDazu;
+			}
+			if(RessourceType.Stein){
+				steinTragend += neuDazu;
+			}
+			if(RessourceType.Nahrung){
+				nahrungTragend += neuDazu;
+			}
+			trageStatus += neuDazu;
+			erfolg = true;
 		}
 		return erfolg;
 	}
 
-	public bool SetHolzTragend(int holzNeuDazu){
-		bool erfolg = false;
-		if ((trageStatus + holzNeuDazu) <= kapazitaet && (trageStatus + holzNeuDazu) >= 0) {
-			holzTragend += holzNeuDazu;
-			trageStatus += holzNeuDazu;
-			erfolg = true;
-		}
-		return erfolg;
-	}
-	public bool SetSteinTragend(int steinNeuDazu){
-		bool erfolg = false;
-		if ((trageStatus + steinNeuDazu) <= kapazitaet && (trageStatus + steinNeuDazu) >= 0) {
-			steinTragend += steinNeuDazu;
-			trageStatus += steinNeuDazu;
-			erfolg = true;
-		}
-		return erfolg;
-	}
-	public bool SetNahrungTragend(int nahrungNeuDazu){
-		bool erfolg = false;
-		if ((trageStatus + nahrungNeuDazu) <= kapazitaet && (trageStatus + nahrungNeuDazu) >= 0) {
-			nahrungTragend += nahrungNeuDazu;
-			trageStatus += nahrungNeuDazu;
-			erfolg = true;
-		}
-		return erfolg;
-	}
 
 
 	// Update is called once per frame
