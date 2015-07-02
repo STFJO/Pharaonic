972949fcfa139dd0773723c7bc2299a48e00ad57
unknown <seminar@FNF-LAP-R9V2XZ0.fnf-gm.intern>
2015-07-02 12:03:53 +0200

    no message

--- Assets/Scripts/IBuilding.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-public interface IBuilding {
-
-
-	Gebäudetyp GetBuildingType();
-	Transform GetTransform();
-}
--- Assets/Scripts/ILagerhaus.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public interface ILagerhaus {
-
-	void Lagern(INPC character);
-
-}
