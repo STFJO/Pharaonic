a0c24ef7c10a25377520f931f22be3607dc6458f
Shanksum <mariusnisslmuller@yahoo.de>
2015-07-03 09:23:32 +0200

    DBRessources

---
+++ Assets/Scripts/DBRessources.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DBRessources {
+
+	private Dictionary<RessourceType, int> ressourceStorage;
+	private static DBRessources self;
+
+	private DBRessources(){
+		ressourceStorage = new Dictionary<RessourceType, int>();
+	}
+
+	public static DBRessources GetInstance(){
+		if(self == null){
+			self = new DBRessources();
+		}
+		return self;
+	}
+
+	public void AddRessourceToDB(RessourceType pRessource, int pMass){
+		ressourceStorage.Add(pRessource, pMass);
+	}
+
+	public int GetValueOf(RessourceType pRessource){
+		int cargoInDB = 0;
+		ressourceStorage.TryGetValue(pRessource, cargoInDB);
+		return cargoInDB;
+	}
+}
