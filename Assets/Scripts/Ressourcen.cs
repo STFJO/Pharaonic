07d234f9c2a0f4fb3371c0a1cb9497e0fa7ada3b
unknown <seminar@FNF-LAP-R9V2XZ0.fnf-gm.intern>
2015-07-04 11:47:44 +0200

    no message

---
+++ Assets/Scripts/Ressourcen.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Ressourcen : MonoBehaviour {
+
+	public string ressource;
+	private string anzahl;
+	private string ausgabe;
+	private Text text;
+	private float abstand = 100f;
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text> ();
+		transform.Translate (-abstand, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		anzahl = "";
+		ausgabe = ressource + ": " + anzahl;
+		text.text = ausgabe;
+	}
+}
